e514d8f18a8cc3e341972b01870c2dc35e9ddd0a
Lewis <lewis.kagia@student.moringaschool.com>
2023-08-15 13:43:16 +0300

    completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
+def happy_new_year(countdown):
     # code goes here!
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+
+happy_new_year()
+
     pass
 
 def square_integers(int_list):
     # code goes here!
+    squared_list = [num ** 2 for num in numbers]
+    return squared_list
+
+# Example usage
+input_list = [1, 2, 3, 4, 5]
+squared_result = square_integers(input_list)
+print(squared_result)  # Output: [1, 4, 9, 16, 25]
+
     pass
 
 def fizzbuzz():
